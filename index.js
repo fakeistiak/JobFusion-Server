c34d2bfe09fe04e63d38662333b7c9f6266910b9
fakeistiak <istiakahmedb100.com>
2025-02-06 22:29:15 +0600

    single and multiple data sent to the client site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
 
@@ -31,10 +31,18 @@ async function run() {
 
 
     const jobsCollection=client.db("jobsDB").collection("jobs");
+
     app.get("/jobs",async(req,res)=>{
       const cursor=jobsCollection.find()
       const result=await cursor.toArray();
       res.send(result)
+    })
+
+    app.get("/jobs/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const job =await jobsCollection.findOne(query)
+      res.send(job)
     })
 
     app.post("/jobs",async(req,res)=>{
