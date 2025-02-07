bf7c6e5b497930c5023cf84ff1b3f5b86c69c866
fakeistiak <istiakahmedb100.com>
2025-02-07 21:35:05 +0600

    .env added and id password set with process.env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
@@ -9,11 +10,13 @@ app.use(cors());
 app.use(express.json());
 
 
-//jobFusion 
-//mViEPEodRgqV3sca
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m8c8ayj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -54,7 +57,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
